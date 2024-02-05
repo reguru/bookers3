9231ed37a42f93742367de4053640675cb34f531
EC2 Default User <ec2-user@ip-172-31-21-65.ap-northeast-1.compute.internal>
2024-02-05 17:38:35 +0000

    投稿,詳細・一覧画面,削除機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,42 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
+    @book = Book.new
+    @user_book = Book.find(params[:id])
+    @user = @user_book.user
+    
   end
 
   def index
+    @book = Book.new
+    @user = current_user
+    @books = Book.all
   end
 
   def edit
+  end
+  
+  def update
+    
+  end 
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body,:image)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
+    @user_books = @user.books
   end
 
   def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def edit
+  end
+  
+  def update
+    
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
